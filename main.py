9d03ee5f91b889e54974ed9cf676453361d0ca8f
sandeep-chahal <sandeepchahal573@gmail.com>
2020-04-08 17:03:19 +0530

    add fetch_data method

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+
+
+def fetch_data():
+    url = 'https://www.worldometers.info/coronavirus/'
+    req = requests.get(url)
+    if(req.status_code != 200):
+        print("something wen wrong", req.status_code)
+        return
+    soup = BeautifulSoup(req.text, "html.parser")
+    rows = soup.find("tbody").find_all("tr")
+    data = {}
+    regex = r"[+,/s]*"
+    for row in rows:
+        td = row.find_all("td")
+        region = td[0].text
+        data[region] = {}
+        data[region]["total_cases"] = re.sub(regex, "", td[1].text)
+        data[region]["new_cases"] = re.sub(regex, "", td[2].text)
+        print(region, re.sub(regex, "", td[4].text))
+        data[region]["total_deaths"] = re.sub(regex, "", td[3].text)
+        data[region]["new_deaths"] = re.sub(regex, "", td[4].text)
+        data[region]["total_recovered"] = re.sub(regex, "", td[5].text)
+        data[region]["active_cases"] = re.sub(regex, "", td[6].text)
+        data[region]["serious"] = re.sub(regex, "", td[7].text)
+    return data
+
+
+fetch_data()
